47e1a5c93b7508e02dd657c8eda8ff9310eae804
Adnan <32223063+AdnanIT@users.noreply.github.com>
2019-02-16 22:17:45 +0100

    Add files via upload

---
+++ Reminder Ispit - Februar 2018/Ispit.Data/EntityModels/StanjeObaveze.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ispit.Data.EntityModels {
+
+    public class StanjeObaveze {
+        public int Id { get; set; }
+        public DateTime DatumIzvrsenja { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Opseg mora biti između 0 i 100.")]
+        [Display(Name = "Izršeno procentualno")]
+        public float IzvrsenoProcentualno { get; set; }
+        public int NotifikacijaDanaPrije { get; set; }
+        public bool NotifikacijeRekurizivno { get; set; }
+        public bool IsZavrseno { get; set; }
+
+
+        [ForeignKey(nameof(Obaveza))]
+        public int ObavezaID { get; set; }
+        public Obaveza Obaveza { get; set; }
+
+
+
+
+        [ForeignKey(nameof(OznacenDogadjaj))]
+        public int OznacenDogadjajID { get; set; }
+        public OznacenDogadjaj OznacenDogadjaj { get; set; }
+
+
+
+
+    }
+}
